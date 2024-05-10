9010a1b3c764862186d8d99d306bc0108e45621c
isaacreid <zac336@hotmail.com>
2024-05-10 00:19:20 -0400

    bdd scenarios

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -35,7 +35,6 @@ def step_impl(context):
     """ Delete all Products and load new ones """
     #
     # List all of the products and delete them one by one
-    #
     rest_endpoint = f"{context.base_url}/products"
     context.resp = requests.get(rest_endpoint)
     assert(context.resp.status_code == HTTP_200_OK)
@@ -45,8 +44,15 @@ def step_impl(context):
 
     #
     # load the database with new products
-    #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
+        
+
